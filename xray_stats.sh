33b591ec0cb827e90c71683d8029a8dcde80cc91
ALAMIN MIA <110433176+A-l-amin@users.noreply.github.com>
2026-09-11 17:20:19 -0800

    Add files via upload

--- xray_stats.sh
+++ xray_stats.sh
@@ -2,44 +2,30 @@
 ## xray api + mysql stats script
 
 # Xray API Config
-_APISERVER=127.0.0.1:62789
-
-# Try to automatically locate Xray binary
-_XRAY=$(command -v xray || true)
-if [[ -z "$_XRAY" ]]; then
-    echo "Error: Xray binary not found. Please install Xray or specify its path."
-    exit 1
-fi
-
-datenow=$(date +"%Y-%m-%d %T")
-server_ip=SERVER_IP
+_APISERVER=127.0.0.1:10000
+_XRAY=/usr/sbin/xray
 
 # MySQL Config
 . /etc/xray/.db-base
 
-apidata() {
+apidata () {
     local ARGS=
     if [[ $1 == "reset" ]]; then
         ARGS="reset: true"
     fi
-
     $_XRAY api statsquery --server=$_APISERVER "${ARGS}" \
     | awk '{
         if (match($1, /"name":/)) {
-            f=1;
-            gsub(/^"|",?$/, "", $2);
-            split($2, p, ">>>");
-            printf "%s:%s->%s\t", p[1], p[2], p[4];
+            f=1; gsub(/^"|link"|,$/, "", $2);
+            split($2, p,  ">>>");
+            printf "%s:%s->%s\t", p[1],p[2],p[4];
         }
-        else if (match($1, /"value":/) && f) {
-            f=0;
+        else if (match($1, /"value":/) && f){
+            f = 0;
             gsub(/"/, "", $2);
             printf "%.0f\n", $2;
         }
-        else if (match($0, /}/) && f) {
-            f=0;
-            print 0;
-        }
+        else if (match($0, /}/) && f) { f = 0; print 0; }
     }'
 }
 
@@ -48,12 +34,11 @@ print_sum() {
     local PREFIX="$2"
     local SORTED=$(echo "$DATA" | grep "^${PREFIX}" | sort -r)
     local SUM=$(echo "$SORTED" | awk '
-        /->up/ { us += $2 }
-        /->down/ { ds += $2 }
-        END {
-            printf "SUM->up:\t%.0f\nSUM->down:\t%.0f\nSUM->TOTAL:\t%.0f\n", us, ds, us + ds;
+        /->up/{us+=$2}
+        /->down/{ds+=$2}
+        END{
+            printf "SUM->up:\t%.0f\nSUM->down:\t%.0f\nSUM->TOTAL:\t%.0f\n", us, ds, us+ds;
         }')
-
     echo -e "${SORTED}\n${SUM}" \
     | numfmt --field=2 --suffix=B --to=iec \
     | column -t
@@ -65,7 +50,7 @@ process_users_to_mysql() {
 
     while IFS=$'\t' read -r line; do
         user=$(echo "$line" | awk -F'[:-> \t]+' '/^user:/ {print $2}')
-        user="${user%-}"
+		user="${user%-}"
         dir=$(echo "$line" | awk -F'[:-> \t]+' '/^user:/ {print $3}')
         val=$(echo "$line" | awk '{print $2}')
 
@@ -84,37 +69,31 @@ process_users_to_mysql() {
         up=${UP[$user]:-0}
         down=${DOWN[$user]:-0}
 
-        mysql --ssl-verify-server-cert=OFF -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" <<EOF
-UPDATE users
-SET
-  xray_upload = xray_upload + $up,
-  xray_download = xray_download + $down,
-  device_connected = 1,
-  is_connected_xray = 1,
-  is_connected = 1,
-  active_address = '$server_ip',
-  active_date = '$datenow'
+        mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" <<EOF
+UPDATE users 
+SET 
+  xray_upload = xray_upload + $up, 
+  xray_download = xray_download + $down 
 WHERE user_name = '$user';
 EOF
     done
 }
 
-# ---------------- MAIN -----------------
 
-DATA=$(apidata "$1")
+
+# Main execution
+DATA=$(apidata $1)
 
 echo "------------Inbound----------"
 print_sum "$DATA" "inbound"
 echo "-----------------------------"
-
 echo "------------Outbound---------"
 print_sum "$DATA" "outbound"
 echo "-----------------------------"
-
 echo
 echo "-------------User------------"
 print_sum "$DATA" "user"
 echo "-----------------------------"
 
-# **IMPORTANT: MySQL update must be called**
-process_users_to_mysql
+# Send per-user stats to MySQL
+process_users_to_mysql "$DATA"
